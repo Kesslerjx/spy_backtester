c087214e06c4d5370d6f0e85c538c558d46cadef
Joseph Kessler <kesslerjx@icloud.com>
2022-09-05 11:26:07 -0400

    Add function to call eod API and returns days

--- files/csv_reader.py
+++ files/csv_reader.py
@@ -1,4 +1,5 @@
 import csv
+import requests
 
 class Day:
     def __init__(self, date, open, high, low, close, adjusted, volume) -> None:
@@ -10,14 +11,29 @@ class Day:
         self.adjusted = float(adjusted)
         self.volume   = float(volume)
 
-def get_days(path):
-    days =[]
+def get_days_from(path):
 
     with open(path, 'r') as infile:
         reader = csv.reader(infile)
         header = next(reader)
+        days   = create_days(reader)
 
-        for row in reader:
+    return days
+
+def get_eod_data(from_date: str, to_date: str, api_key: str):
+    API_CALL = 'https://eodhistoricaldata.com/api/eod/MCD.US?api_token=' + api_key + '&from=' + from_date + '&to=' + to_date
+    response = requests.get(API_CALL)
+    lines    = response.text.splitlines()
+    reader   = csv.reader(lines)
+    header   = next(reader)
+    days     = create_days(reader)
+
+    return days
+
+def create_days(reader):
+    days = []
+
+    for row in reader:
             days.append(Day(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
-
+    
     return days
--- files/tester.py
+++ files/tester.py
@@ -1,9 +1,10 @@
-from csv_reader import get_days
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from csv_reader import get_days_from, get_eod_data
 from stock_analyzer import print_list, get_avg_range, get_best_nday, map_best_ndays, trend_trade_chance_ndays, best_trend_trade_chance, days_before_opp_dir
 from stock_trader import trade_ndays, get_best_trade_results
 
-stock_days = get_days('./data/spy_daily_since_2022.csv')
-best_ndays = map_best_ndays(stock_days)
-avg_days   = days_before_opp_dir(stock_days)
-
-print(avg_days)
+# Get data 3 years from today
+to_date   = date.today().strftime('%Y-%m-%d')
+from_date = (date.today() - relativedelta(years=3)).strftime('%Y-%m-%d')
+days      = get_eod_data(from_date, to_date, '')
